826831ba9e8ca812766df4d3be423de3544e0901
Mojtaba Farahani <farahani455@yahoo.cm>
2020-02-18 01:16:48 +0330

    Init Chat app

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const publicPath = path.join(__dirname,"../public");
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const port = process.env.PORT || 3000;
+ 
+const app=  express();
+app.use( express.static(publicPath));
+var server = http.createServer(app);
+var io= socketIO(server);
+io.on("connection",(socket)=>{
+    console.log("User Connected");
+
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected");
+    })
+})
+
+server.listen(port,()=>{
+    console.log(`Start in Port ${port}`)
+})
